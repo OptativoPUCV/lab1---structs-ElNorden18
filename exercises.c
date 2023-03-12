02cd3b6b74e6a5256f1886ed406a1eab7d94915a
Replit user <>
2023-03-12 00:02:15 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -80,7 +80,7 @@ typedef struct {
 } Vector;
 
 Vector * crearVector(int n) {
-  Vector *vector = (Persona *) calloc(sizeof(Persona));
+  Vector *vector = (Persona *) calloc(int n ,sizeof(Persona));
    return NULL;
 }
 
